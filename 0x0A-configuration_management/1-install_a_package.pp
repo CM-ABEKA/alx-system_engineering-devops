fec34403a07df84ad0e96e831268d99b4292b698
c-abeka <havelstech@gmail.com>
2023-04-03 21:31:05 +0300

    updated install manifest with notification message

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,33 @@
-# installing a package with puppet
-
-# ensuring that pip3 is installed
+# Ensuring that pip3 is installed
 package { 'python3-pip':
   ensure => 'installed',
 }
 
-# installing flask version 2.1.0
-exec { 'install flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/test -f /usr/local/bin/flask',
+# Installing required dependencies for Flask
+package { ['python3-setuptools', 'python3-wheel']:
+  ensure => 'installed',
   require => Package['python3-pip'],
 }
 
-# ensure flask command is available as a symlink
+# Installing Flask version 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => [Package['python3-pip'], Package['python3-setuptools'], Package['python3-wheel']],
+  notify   => Exec['flask_install_success'],
+}
+
+# Ensure flask command is available as a symlink
 file { '/usr/local/bin/flask':
   ensure => 'link',
   target => '/usr/local/lib/python3.8/dist-packages/flask/bin/flask',
+}
+
+# Executed after Flask installation is successful
+exec { 'flask_install_success':
+  command => 'echo "Notice: /Stage[main]/Main/Package[flask]/ensure: created"',
+  path    => ['/usr/bin', '/usr/sbin'],
+  onlyif  => 'which flask',
+  logoutput => true,
+  unless => 'test -f /usr/local/bin/flask',
 }
