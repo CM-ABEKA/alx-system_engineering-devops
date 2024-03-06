7e876e20a6cdeae336697e791c1fb1c339ad2152
CM-ABEKA <cmabeka2016@gmail.com>
2024-03-07 00:23:50 +0300

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/python3
+
+"""
+Function returning  number of subscribers in a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    This function returns the number of subscribers in a subreddit
+    """
+
+    # Default Urls
+    api_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    res = requests.get(api_url,
+                        headers={'User-Agent': 'ALX - 0x16.advanced.api'},
+                        allow_redirects=False
+                        )
+
+    if res.status_code in [302, 404]:
+        return 0
+
+    return res.json().get('data').get('subscribers')
