0ed6c673e5326ca39ec738be8cb77d0256ccaef2
CM-ABEKA <cmabeka2016@gmail.com>
2024-03-07 01:26:57 +0300

    Using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Puppet manifest to change ssh config file.
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+	content => "
+		#SSH client configuration
+		host*
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no
+		"
+}
