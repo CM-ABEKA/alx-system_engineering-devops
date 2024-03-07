c791a6542349ef2b881f25d82905621fea142420
CM-ABEKA <cmabeka2016@gmail.com>
2024-03-07 23:21:08 +0300

    subrettits get

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,9 +14,8 @@ def number_of_subscribers(subreddit):
     # Default Urls
     api_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     res = requests.get(api_url,
-                        headers={'User-Agent': 'ALX - 0x16.advanced.api'},
-                        allow_redirects=False
-                        )
+                       headers={'User-Agent': 'ALX - 0x16.advanced.api'},
+                       allow_redirects=False)
 
     if res.status_code in [302, 404]:
         return 0
