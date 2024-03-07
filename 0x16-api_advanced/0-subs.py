2d862bcd5aa80b0b5f62caa5edba7accc9b94225
CM-ABEKA <cmabeka2016@gmail.com>
2024-03-07 23:39:41 +0300

    github update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,15 @@ import requests
 def number_of_subscribers(subreddit):
     """
     This function returns the number of subscribers in a subreddit
+    - if not a valid redit return 0
     """
 
     # Default Urls
     api_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/cmabe_'}
+    headers = {'User-Agent': 'info@leetwork.com'}
     res = requests.get(api_url, headers, allow_redirects=False)
 
-    if res.status_code >= 400:
+    if res.status_code == 200:
+        return res.json().get('data').get('subscribers')
+    else:
         return 0
-
-    return res.json().get('data').get('subscribers')
