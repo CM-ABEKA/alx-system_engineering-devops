4f54b8f9b64071af89b5ea8f4435fe21fb280cab
CM-ABEKA <cmabeka2016@gmail.com>
2024-03-09 18:58:43 +0300

    puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix Error in file extension in wp-settings ".phpp"
 
 exec { 'fixWpSettigsTypo' :
-	command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path	=> '/usr/local/bin:/bin'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin:/bin'
 }
